a1ca4001712cd6609c70de471357f21dc774011b
Elijah Ki-Zerbo <58334072+Waranika@users.noreply.github.com>
2024-02-24 16:04:38 +0100

    Added Webpage, Flask API and transformer

---
+++ Web/app.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append(r"C:\Users\kizer\Kasa\Web")
+
+from flask import Flask, request, jsonify
+from eng_bam_transformer.py import predict
+
+app = Flask(__name__)
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    try:
+        # Get the input text from the request
+        data = request.get_json()
+        test_sequence = data.get('text')
+
+        # Perform translation
+        _, _, _, input_words, output_words = predict(test_sequence)
+
+        # Return the translated text
+        translated_text = ' '.join(output_words[:-1])  # Exclude <end> token
+        return jsonify({"translated_text": translated_text})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
